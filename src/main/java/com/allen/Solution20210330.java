56bc8c1a23e65035f3625d9f64cb39b9707d470d
wangshulun <799895929@qq.com>
2021-03-30 10:27:11 +0800

    290 颠倒二进制位

---
+++ src/main/java/com/allen/Solution20210330.java
@@ -0,0 +1,47 @@
+package com.allen;
+
+/**
+ * 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+ * <p>
+ * 每行中的整数从左到右按升序排列。
+ * 每行的第一个整数大于前一行的最后一个整数。
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+ * 输出：true
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-a-2d-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author wangshulun
+ * @date 2021/3/30
+ */
+public class Solution20210330 {
+    public static void main(String[] args) {
+
+    }
+
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int low = 0, high = m * n - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int x = matrix[mid / n][mid % n];
+            if (x < target) {
+                low = mid + 1;
+            } else if (x > target) {
+                high = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
